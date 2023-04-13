9063e045732bcea63518714a81592562a4e0b9ce
colelevi <christensencole4@gmail.com>
2023-04-13 15:08:43 -0600

    db stuff

---
+++ Models/Migrations/20230413202103_security.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntexFagElGamous.Models.Migrations
+{
+    public partial class security : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
