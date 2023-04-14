46927c5fb388e7aeba4f1468f73ec8b27fedda78
colelevi <christensencole4@gmail.com>
2023-04-13 19:13:55 -0600

    cookies

--- Views/Shared/_CookieConsentPartial.cshtml.cs
+++ Views/Shared/_CookieConsentPartial.cshtml.cs
@@ -1,12 +1,27 @@
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Mime;
 
 namespace IntexFagElGamous.Views.Shared
 {
     public class _CookieConsentPartialModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            var consentFeature = HttpContext.Features.Get<ITrackingConsentFeature>();
+            if (consentFeature?.CanTrack ?? false)
+            {
+                // Cookies are allowed, no need to show the banner
+                return new EmptyResult();
+            }
+            else
+            {
+                // Cookies are not allowed, show the banner
+                var cookieString = consentFeature?.CreateConsentCookie();
+                ViewData["CookieString"] = cookieString;
+                return Page();
+            }
         }
     }
 }
