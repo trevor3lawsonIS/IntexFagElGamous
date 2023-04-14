d8551d9606f3fb2dcc59ab9f8814192b507d93f0
Josh Blad <94322381+jblad99@users.noreply.github.com>
2023-04-13 22:04:45 -0600

    Updated roles, identities, other stuff

---
+++ EditRoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace IntexFagElGamous.Models.ViewModels
+{
+	public class EditRoleViewModel
+	{
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
+
